236156795effb0a462086745f7ba44e94609e712
扶苍 <zhuzai2406@qq.com>
2018-07-31 10:47:49 +0800

    第一版

--- git/src/main/java/testgit/App.java
+++ git/src/main/java/testgit/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "test github!" );
+        System.out.println( "test github!" );
+
+        System.out.println( "test github!" );
+
+
     }
 }
